41ba750683c2d9613586bcedbd5538e5e1a06b4e
Andrey <silovaa@gmail.com>
2026-01-08 20:44:54 +0300

    to notebook

--- egl/package.d
+++ egl/package.d
@@ -5,12 +5,14 @@ public import egl_import;
 import std.exception;
 import std.typecons: Tuple, tuple;
 
-struct Context
+struct DisplayExt(string tag)
 {
-    Tuple!(int, int) initialize(EGLenum platform, 
+    static Tuple!(int, int) initialize(EGLenum platform, 
                                 void* native_display, 
                                 const(EGLint)* attrib_list = null)
     {
+        assert(m_display == EGL_NO_DISPLAY, "m_display is already initialized");
+
         auto eglGetPlatformDisplayEXT =
             cast(PFNEGLGETPLATFORMDISPLAYEXTPROC)eglGetProcAddress("eglGetPlatformDisplayEXT");
         enforce(eglGetPlatformDisplayEXT, "eglGetPlatformDisplayEXT not supported");
@@ -26,17 +28,68 @@ struct Context
         return tuple(majorVersion, minorVersion);
     }
 
-    @disable this(this);
-
-    ~this()
+    static void terminate() nothrow @nogc
     {
-        
         if (EGL_NO_DISPLAY != m_Display) 
             eglTerminate(m_Display);
     }
 
 private:
-    EGLDisplay m_display;
+    static EGLDisplay m_display = EGL_NO_DISPLAY;
+
+    //static Display s_inst;
+}
+
+alias Display = DisplayExt!null;
+
+struct Context
+{
+    this(string tag)(const(uint)[] configAttribs, const(uint)[] contextAttribs)
+    {
+        config(DisplayExt!tag.m_display, configAttribs, contextAttribs);    
+    }
+
+    this(const(uint)[] configAttribs, const(uint)[] contextAttribs)
+    {
+        config(Display.m_display, configAttribs, contextAttribs);
+    }
+
+    @disable this(this);
+
+    ~this()
+    {
+        
+    }
+
+private:
     EGLContext m_context;
     EGLSurface m_surface;
+
+    void config(EGLDisplay dpy, const(uint)[] configAttribs, const(uint)[] contextAttribs)
+    {
+
+    }
+}
+
+struct Config
+{
+    this(string tag)(const(uint)[] configAttribs)
+    {
+        construct(DisplayExt!tag.m_display, configAttribs);    
+    }
+
+    this(const(uint)[] configAttribs)
+    {
+        construct(Display.m_display, configAttribs);
+    }
+
+private:
+    EGLConfig m_config;
+
+    void construct(EGLDisplay dpy, const(uint)[] configAttribs)
+    {
+        EGLint numConfigs;
+        enforce(eglChooseConfig(dpy, configAttribs.ptr, &m_config, 1, &numConfigs) == 0,
+                "Could not create choose config!");
+    }
 }
